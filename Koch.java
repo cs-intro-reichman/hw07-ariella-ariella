cc0102fa78979c96030ce7edf5e07ca4c3b5c104
Ariella Stefansky <ariella.stefansky@post.runi.ac.il>
2026-01-02 15:14:32 +0200

    Completed assignment

--- Koch.java
+++ Koch.java
@@ -8,14 +8,13 @@ public class Koch {
 		//// Uncomment only one block in each test, and remember to compile
 		//// the class whenever you change the test.
 
-        /*
 		// Tests the curve function:
 		// Gets n, x1, y1, x2, y2,
 		// and draws a Koch curve of depth n from (x1,y1) to (x2,y2).
 		curve(Integer.parseInt(args[0]),
 			  Double.parseDouble(args[1]), Double.parseDouble(args[2]), 
 		      Double.parseDouble(args[3]), Double.parseDouble(args[4]));
-		*/
+		
 
 		/*
 		// Tests the snowflake function:
@@ -27,8 +26,24 @@ public class Koch {
 	/** Gets n, x1, y1, x2, y2,
      *  and draws a Koch curve of depth n from (x1,y1) to (x2,y2). */
 	public static void curve(int n, double x1, double y1, double x2, double y2) {
-		StdDraw.line(x1, y1, x2, y2);
-		//// Write the rest of your code below.
+		if (n == 0) {
+			StdDraw.line(x1, y1, x2, y2);
+			return;
+		}
+		double xB = (2 * x1 + x2) / 3.0;
+		double yB = (2 * y1 + y2) / 3.0;
+	
+		double xC = (x1 + 2 * x2) / 3.0;
+		double yC = (y1 + 2 * y2) / 3.0;
+
+		double root3Over6 = Math.sqrt(3.0) / 6.0;
+		double xE = root3Over6 * (yB - yC) + 0.5 * (xB + xC);
+		double yE = root3Over6 * (xC - xB) + 0.5 * (yB + yC);
+
+		curve(n - 1, x1, y1, xB, yB);
+		curve(n - 1, xB, yB, xE, yE);
+		curve(n - 1, xE, yE, xC, yC);
+		curve(n - 1, xC, yC, x2, y2);
 	}
 
     /** Gets n, and draws a Koch snowflake of n edges in the standard canvass. */
--- Sierpinski.java
+++ Sierpinski.java
@@ -7,12 +7,33 @@ public class Sierpinski {
 	
 	// Draws a Sierpinski triangle of depth n on the standard canvass.
 	public static void sierpinski (int n) {
-		//// Replace this comment with your code
+		StdDraw.setCanvasSize();
+		StdDraw.setScale(0, 1.0);
+		StdDraw.line(0, 0, 1, 0);
+		StdDraw.line(0, 0, 0.5, Math.sqrt(3) / 2.0);
+		StdDraw.line(1, 0, 0.5, Math.sqrt(3) / 2.0);
+		sierpinski(n, 0, 1, 0.5, 0, 0, Math.sqrt(3) / 2.0);
 	}
 	
 	// Does the actual drawing, recursively.
 	private static void sierpinski(int n, double x1, double x2, double x3,
 		                                 double y1, double y2, double y3) {
-		//// Replace this comment with your code
+		if (n == 0) return;
+		double mx12 = (x1 + x2) / 2.0;
+		double my12 = (y1 + y2) / 2.0;
+	
+		double mx23 = (x2 + x3) / 2.0;
+		double my23 = (y2 + y3) / 2.0;
+	
+		double mx31 = (x3 + x1) / 2.0;
+		double my31 = (y3 + y1) / 2.0;
+
+		StdDraw.line(mx12, my12, mx23, my23);
+		StdDraw.line(mx23, my23, mx31, my31);
+		StdDraw.line(mx31, my31, mx12, my12);
+
+		sierpinski(n - 1, x1, mx12, mx31, y1, my12, my31);
+		sierpinski(n - 1, mx12, x2, mx23, my12, y2, my23);
+		sierpinski(n - 1, mx31, mx23, x3, my31, my23, y3);
 	}
 }
